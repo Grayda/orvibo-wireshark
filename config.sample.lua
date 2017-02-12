12ff96207fb7bb44b40863773fb56f963c592394
David Gray <grayda@solidinc.org>
2017-02-12 17:42:55 +1100

    More Orvibo ports

--- config.sample.lua
+++ config.sample.lua
@@ -1,4 +1,15 @@
 -- Place your key here
 key = "yourkeygoeshere1"
 -- Ports to register with Wireshark
-ports = { 10000, 9999, 48899, 49999, 37832, 8295 }
+ports = {
+  8295,
+  9999,
+  10000,
+  10001,
+  37832,
+  39338,
+  39344,
+  39387,
+  48899,
+  49999
+}
